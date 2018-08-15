0d8c2d33e5f538a780450e1d8781e580764ae2f8
Gustavo Britto <gustavobrittogebrim@gmail.com>
2018-08-14 22:58:53 -0300

    fix tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+  ],
+  testURL: 'http://localhost',
+  verbose: true,
+  moduleNameMapper: {
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^app/(.*)$': '<rootDir>/$1',
+  },
+};
